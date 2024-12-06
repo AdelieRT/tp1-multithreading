c0923ae14d3868bc7cd512ffa7916777ce04969d
Adélie <adelie@rasseneur-tricard@univ-tlse3.fr>
2024-12-06 17:30:30 +0100

    avancé tp4

--- low_level.cpp
+++ low_level.cpp
@@ -11,14 +11,16 @@
 // puis resoudre a*x=b avec eigen à la place de numpy comme précéddemment
 // refaire une class cpp pour Task
 // pour voir le res du proxy aller à l'adresse http://localhost:8000/
+//mettre des commentaires de ce que l'on cherche à faire pas de ce que le code fait
 class Task {
 public:
   int identifier;
   int size;
   std::chrono::time_point<std::chrono::steady_clock> end;
   std::chrono::time_point<std::chrono::steady_clock> start;
-  //duration in nanoseconds
+  //temps de tache en nanoseconds
   std::chrono::duration<long int, std::ratio<1, 1000000000>> duration;
+  float time;
   //use matrixxf with eigen lib
   Eigen::MatrixXf a;
   Eigen::MatrixXf b;
@@ -35,6 +37,7 @@ public:
     this->b = Eigen::MatrixXf::Random(this->size, 1);
     //initialisation de x en matrice[size, 1]=[0....0]
     this->x = Eigen::MatrixXf::Zero(this->size, 1);
+    this->time = 0;
   }
 
   void work() {
@@ -42,11 +45,38 @@ public:
     this->x = this->b * this->a.inverse();
     this->end = std::chrono::steady_clock::now();
     this->duration = this->end - start;
+    this->time = this->duration.count();
   }
 
   std::string to_json() {
+    nlohmann::json task_json;
 
-    return 0;
+    nlohmann::json a_array = nlohmann::json::array();
+    for(int i=0; i<this->size; i++){
+      for(int j=0; j<this->size; j++){
+        a_array.push_back(this->a.coeff(i,j));
+      }
+    }
+
+    nlohmann::json b_array = nlohmann::json::array();
+    for(int i=0; i<this->size; i++){
+      b_array.push_back(this->b.coeff(i,1));
+    }
+
+    nlohmann::json x_array = nlohmann::json::array();
+    for(int i=0; i<this->size; i++){
+      x_array.push_back(this->x.coeff(i,1));
+    }
+    
+
+    task_json["identifier"] = this->identifier;
+    task_json["size"] = this->size;
+    task_json["a"] = a_array;
+    task_json["b"] = b_array;
+    task_json["x"] = x_array;
+    task_json["time"] = this->time;
+
+    return task_json.dump();
   }
 
   Task from_json(std::string text) {
@@ -61,9 +91,14 @@ public:
 
 int main(int argc, char **argv) {
   Task t = Task();
-  //std::cout << t.x << std::endl;
   t.work();
-  //std::cout << (t.duration.count())/1e9 << " sec" << std::endl;
+  //durée de taches en seconde
+  std::cout << (t.time)/1e9 << " sec" << std::endl;
+  std::string t_to_j = t.to_json();
+  //task to json
+  std::cout << t_to_j << std::endl;
+
+  //dans l'autre sens il faudra couper à chaque fois qu'on atteindra la size
 
 
   cpr::Response r = cpr::Get(cpr::Url{"http://localhost:8000/"});
